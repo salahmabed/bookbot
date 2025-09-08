99bfbf96c8e161182093bf6f2b4a4fb19d2d42d8
salahmabed <salah.m.abed@gmail.com>
2025-09-08 16:47:57 -0700

    Variable renaming.

--- stats.py
+++ stats.py
@@ -1,12 +1,12 @@
 def sort_on(items):
     return items["num"]
 
-def bag_dicts(dict):
+def bag_dicts(some_dict):
     bag_of_dicts = []
-    for entry in dict:
+    for this_name, this_num in some_dict.items():
         this_dict = {}
-        this_dict["name"] = entry
-        this_dict["num"] = dict[entry]
+        this_dict["name"] = this_name
+        this_dict["num"] = this_num
         bag_of_dicts.append(this_dict)
 
     return bag_of_dicts
@@ -26,8 +26,8 @@ def get_num_chars(string_of_words):
     
     return char_dict
 
-def dict_to_sorted_array(dict):
-    char_dict = get_num_chars(dict)
+def dict_to_sorted_array(some_dict):
+    char_dict = get_num_chars(some_dict)
 
     dict_bag = bag_dicts(char_dict)
     dict_bag.sort(reverse=True, key=sort_on)
