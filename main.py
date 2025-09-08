a6715e36a08c5d79988d78bb1dcf7c3e22ac7295
salahmabed <salah.m.abed@gmail.com>
2025-09-08 16:36:26 -0700

    Generate book report.

--- main.py
+++ main.py
@@ -3,10 +3,24 @@ from stats import *
 def get_book_text(file_path):
     with open(file_path) as f:
         return f.read()
+    
+def book_report(file_path):
+    book_text = get_book_text(file_path)
+    word_count = get_num_words(book_text)
+    char_array = dict_to_sorted_array(book_text)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for char_dict in char_array:
+        if char_dict["name"].isalpha():
+            print(f"{char_dict["name"]}: {char_dict["num"]}")
+    print("============= END ===============")
 
 def main():
     book_address = "books/frankenstein.txt"
-    print(get_num_words(get_book_text(book_address)))
-    print(get_num_chars(get_book_text(book_address)))
+    book_report(book_address)
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,16 @@
+def sort_on(items):
+    return items["num"]
+
+def bag_dicts(dict):
+    bag_of_dicts = []
+    for entry in dict:
+        this_dict = {}
+        this_dict["name"] = entry
+        this_dict["num"] = dict[entry]
+        bag_of_dicts.append(this_dict)
+
+    return bag_of_dicts
+
 def get_num_words(string_of_words):
     return len(string_of_words.split())
 
@@ -10,4 +23,13 @@ def get_num_chars(string_of_words):
             char_dict[str(ch)] += 1
         else:
             char_dict[str(ch)] = 1
+    
     return char_dict
+
+def dict_to_sorted_array(dict):
+    char_dict = get_num_chars(dict)
+
+    dict_bag = bag_dicts(char_dict)
+    dict_bag.sort(reverse=True, key=sort_on)
+
+    return dict_bag
