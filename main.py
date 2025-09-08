66ee493439b88bdac34b27e4bf9d947bde7f8fda
salahmabed <salah.m.abed@gmail.com>
2025-09-08 13:36:44 -0700

    Print entire text.

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("greetings boots")
+def get_book_text(file_path):
+    with open(file_path) as f:
+        return f.read()
+
+def main():
+    print(get_book_text("books/frankenstein.txt"))
+
+main()
