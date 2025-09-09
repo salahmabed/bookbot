462f22debb24aeaaa09d82a9c79a3d6ba070213d
salahmabed <salah.m.abed@gmail.com>
2025-09-08 21:47:48 -0700

    Added docstring to main() too.

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ def book_report(file_path):
     print("============= END ===============")
 
 def main():
+    """Activates and runs the bookbot."""
     if len(sys.argv) != 2:
         print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
